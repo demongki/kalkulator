a4036f8623cd3caa7581770d48b14103a75ce685
demongki <86837780+demongki@users.noreply.github.com>
2021-10-13 15:55:53 +0700

    Add files via upload

---
+++ luas_lingkaran.py
@@ -0,0 +1,9 @@
+// BloodSword
+// BloodSword
+phi = 3.14
+diameter = int(input('masukan diameter: '))
+r = diameter / 2
+luas = phi * (r ** 2)
+print('hasil luas lingkaran adalah: ' + str(luas) +'cm kubik')
+// BloodSword
+// BloodSword
